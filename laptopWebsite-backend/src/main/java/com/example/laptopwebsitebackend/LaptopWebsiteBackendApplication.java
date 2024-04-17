d8a6c7df97ac25a512d21d80298ba73ce4688793
Vu Xuan Hoang <xuanhoang949@gmail.com>
2024-04-17 23:44:43 +0700

    Add backend and connect to database

---
+++ laptopWebsite-backend/src/main/java/com/example/laptopwebsitebackend/LaptopWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.laptopwebsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ laptopWebsite-backend/src/test/java/com/example/laptopwebsitebackend/LaptopWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laptopwebsitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
