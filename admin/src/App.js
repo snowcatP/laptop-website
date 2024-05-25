b90b674323c33c50f7d1ee5550e6a9a4d2e2df43
Vu Xuan Hoang <89919829+snowcatP@users.noreply.github.com>
2024-05-25 18:59:24 +0700

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,56 +1,159 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./client/HomePage";
-import Store from "./client/Store";
-import Product from "./client/Product";
-import Checkout from "./client/Checkout";
-import Login from "./client/Login";
-import Register from "./client/Register";
-import ForgotPassword from "./client/ForgotPassword";
-import UserRoutes from "./client/UserRoutes";
-import Error404 from "./client/Error404";
-import ProtectedRoute from "./client/util/ProtectedRoute";
-import ResetPassword from "./client/ResetPassword";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./admin/Admin";
+import AdminLogin from "./admin/AdminLogin";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import AdminProfile from "./admin/AdminProfile";
+import Error404 from "./admin/Error404";
+import AddProduct from "./admin/AddProduct";
+import ListProduct from "./admin/ListProduct";
+import EditProduct from "./admin/EditProduct";
+import ListWarranty from "./admin/ListWarranty";
+import AddWarranty from "./admin/AddWarranty";
+import ProtectedRoute from "./admin/util/ProtectedRoute";
+import ListDiscount from "./admin/ListDiscount";
+import AddDiscount from "./admin/AddDiscount";
+import EditDiscount from "./admin/EditDiscount";
+import ListCustomer from "./admin/ListCustomer";
+import EditWarranty from "./admin/EditWarranty";
+import ListEmployee from "./admin/ListEmployee";
+import AddEmployee from "./admin/AddEmployee";
+import ListOrder from "./admin/ListOrder";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-
-        <Route path="/auth/login" element={<Login />}></Route>
-        <Route path="/auth/register" element={<Register />}></Route>
-
-        <Route path="/store" element={<Store />}></Route>
-        <Route path="/product/:id" element={<Product />}></Route>
-
-        <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-        <Route path="/reset-password/:token" element={<ResetPassword />}></Route>
-        {/*
-          Protected endpoints
-        */}
+        <Route path="/auth/admin-login" index element={<AdminLogin />} />
 
         <Route
-          path="/user/*"
-          element={
-              <UserRoutes />
-          }
-        ></Route>
-
-        <Route
-          path="/checkout"
+          path="/"
           element={
             <ProtectedRoute>
-              <Checkout />
+              <Admin />
             </ProtectedRoute>
           }
-        ></Route>
+        />
+        <Route
+          path="/admin/profile"
+          element={
+            <ProtectedRoute>
+              <AdminProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-product"
+          element={
+            <ProtectedRoute>
+              <AddProduct />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/list-product"
+          element={
+            <ProtectedRoute>
+              <ListProduct />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit-product/:id"
+          element={
+            <ProtectedRoute>
+              <EditProduct />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/list-warranty"
+          element={
+            <ProtectedRoute>
+              <ListWarranty />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-warranty"
+          element={
+            <ProtectedRoute>
+              <AddWarranty />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit-warranty/:id"
+          element={
+            <ProtectedRoute>
+              < EditWarranty/>
+            </ProtectedRoute>
+          }
+        />
 
-        
+        <Route
+          path="/list-discount"
+          element={
+            <ProtectedRoute>
+              <ListDiscount />
+            </ProtectedRoute>
+          }
+        />
 
-        {/*Error 404 */}
-        <Route path="/*" element={<Error404 />}></Route>
-        
+        <Route 
+          path="/add-discount"
+          element={
+            <ProtectedRoute>
+              <AddDiscount />
+            </ProtectedRoute>
+        }
+        />
+        <Route
+          path="/edit-discount/:id"
+          element={
+            <ProtectedRoute>
+              <EditDiscount />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/list-customer"
+          element={
+            <ProtectedRoute>
+              <ListCustomer />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/list-employee"
+          element={
+            <ProtectedRoute>
+              <ListEmployee />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/add-employee"
+          element={
+            <ProtectedRoute>
+              <AddEmployee />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/list-order"
+          element={
+            <ProtectedRoute>
+              <ListOrder />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/error404" element={<Error404 />} />
+        <Route path="/*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
